c98a31eefdce019eb0d4fce352bb308fef7e3558
Łukasz Bandzarewicz <lucassus@gmail.com>
2010-08-08 17:51:13 +0200

    Printing data.

--- ifin24-client.rb
+++ ifin24-client.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
 require 'rubygems'
 require 'bundler'
@@ -18,19 +19,21 @@ def login(agent, login, password)
   login_form.submit
 end
 
-def fetch_categories(categories_element)
-  categories = {}
+def fetch_categories(agent)
+  categories_element = agent.page.search('ul.expenseCombo>li')
 
+  categories = {}
   categories_element[1..-1].each do |category_elem|
-    next if category_elem.children[1].nil?
+    category_without_children = category_elem.children[1].nil?
+    next if category_without_children
     
     category_name = category_elem.children[0].text.strip
     sub_categories_element = category_elem.children[1].children.search('li')
 
     sub_categories = {}
-    sub_categories_element.each do |sub_cat|
-      sub_name = sub_cat.text.strip
-      id = sub_cat.attributes['rel'].value
+    sub_categories_element.each do |sub_category_elem|
+      sub_name = sub_category_elem.text.strip
+      id = sub_category_elem.attributes['rel'].value
 
       sub_categories[sub_name] = id
     end
@@ -41,12 +44,13 @@ def fetch_categories(categories_element)
   return categories
 end
 
-def fetch_accounts(accounts_element)
-  accounts = {}
+def fetch_accounts(agent)
+  accounts_element = agent.page.search('ul#bankAccountCombo>li')
 
+  accounts = {}
   accounts_element.each do |account_elem|
     id = account_elem.attributes['rel'].value
-    next if id == '0'
+    next if id == '0' # skip the prompt
     name = account_elem.text.strip
 
     accounts[name] = id
@@ -62,7 +66,7 @@ def add_entry(agent, data)
   form['entry.title'] = data[:title].to_s
   form['entry.date'] = data[:date].to_s
   form['selectedBankAccount'] = data[:account_id].to_s
-  form['entry.entryCategory.id'] = data[:category_id].to_s
+  form['entry.entryCategory.id'] = data[:subcategory_id].to_s
   form['entry.amount'] = data[:amount].to_s
   form['entry.note'] = data[:note].to_s
 
@@ -72,11 +76,8 @@ end
 def get_data(agent)
   # Fetch categories and accounts
   agent.get('https://www.ifin24.pl/zarzadzanie-finansami/transakcje/dodaj-wydatek')
-  element = agent.page.search('ul.expenseCombo>li')
-  categories = fetch_categories(element)
-
-  element = agent.page.search('ul#bankAccountCombo>li')
-  accounts = fetch_accounts(element)
+  categories = fetch_categories(agent)
+  accounts = fetch_accounts(agent)
 
   data = {}
   data[:title] = ask('Nazwa')
@@ -90,27 +91,35 @@ def get_data(agent)
     menu.prompt = 'Wybierz rachunek'
 
     accounts.each do |name, id|
-      menu.choice(name) { data[:account_id] = id }
+      menu.choice(name) do
+        data[:account_name] = name
+        data[:account_id] = id
+      end
     end
-
   end
 
   sub_categories = {}
   choose do |menu|
-    menu.prompt = 'Wybierz kategorie'
+    menu.prompt = 'Wybierz kategorię'
 
     categories.each do |name, sub|
-      menu.choice(name) { sub_categories = sub }
+      menu.choice(name) do
+        data[:category_name] = name
+        sub_categories = sub
+      end
     end
   end
 
 
-  data[:category_id] = nil
+  data[:subcategory_id] = nil
   choose do |menu|
-    menu.prompt = 'Wybierz podkategorie'
+    menu.prompt = 'Wybierz podkategorię'
 
     sub_categories.each do |name, id|
-      menu.choice(name) { data[:category_id] = id }
+      menu.choice(name) do
+        data[:subcategory_name] = name
+        data[:subcategory_id] = id
+      end
     end
   end
 
@@ -120,17 +129,54 @@ def get_data(agent)
   return data
 end
 
+def print_data(data)
+  puts "=" * 16
+  puts "Nazwa: #{data[:title]}"
+  puts "Data: #{data[:date]}"
+  puts "Rachunek #{data[:account_name]}"
+  puts "Kwota: #{data[:amount]}"
+  puts "Kategoria: #{data[:category_name]} / #{data[:subcategory_name]}"
+  puts "Opis: #{data[:note]}"
+  puts "=" * 16
+end
+
 def load_config
-  file_name = File.join(File.expand_path(File.dirname(__FILE__)), 'config.yml')
-  YAML.load_file(file_name) rescue {}
+  file_name = File.join(ENV['HOME'], '.ifin24-client', 'config.yml')
+  if File.exist?(file_name)
+    YAML.load_file(file_name) rescue {}
+  else
+    puts "Nie odnaleziono pliku konfiguracyjnego #{file_name}"
+    exit
+  end
 end
 
 def main
   agent = Mechanize.new
+
+  # login
   config = load_config
   login(agent, config[:login], config[:password])
-  data = get_data(agent)
-  add_entry(agent, data)
+
+  # add entry
+  loop do
+    choose do |menu|
+      menu.layout = :menu_only
+      menu.shell  = true
+
+      menu.choices(:dodaj, :add) do
+        data = get_data(agent)
+        print_data(data)
+        if agree("Dane poprawne?")
+          puts "Wysyłanie danych.."
+          add_entry(agent, data)
+        end
+      end
+
+      menu.choices(:koniec, :exit, :q) do
+        exit
+      end
+    end
+  end
 end
 
 main()
