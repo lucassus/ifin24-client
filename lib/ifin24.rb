804c79d422a7ea1c9f1d458a5d67e2f2201f6c42
Łukasz Bandzarewicz <lucassus@gmail.com>
2010-08-24 16:56:32 +0200

    Remove version from main module.

--- lib/ifin24.rb
+++ lib/ifin24.rb
@@ -1,7 +1,4 @@
 module Ifin24
-  # The current version of ifin24.
-  # Do not change the value by hand; it will be updated automatically by the gem release script.
-  VERSION = "0.0.1"
 
   autoload :Client, 'ifin24/client'
   autoload :Commands, 'ifin24/commands'
@@ -9,4 +6,6 @@ module Ifin24
   autoload :Console, 'ifin24/console'
   autoload :Helpers, 'ifin24/helpers'
   autoload :Models, 'ifin24/models'
+
 end
+
