25cb5936c9ba57e9ec2c4b199f24357a27545105
Łukasz Bandzarewicz <lucassus@gmail.com>
2010-08-28 18:27:07 +0200

    Make nice bars for the expense limits.

--- lib/ifin24/commands/list_limits.rb
+++ lib/ifin24/commands/list_limits.rb
@@ -1,10 +1,37 @@
 # encoding: utf-8
 
 class Ifin24::Commands::ListLimits < Ifin24::Commands::Base
+  BAR_SIZE = 50
 
   def execute
     limits = @client.fetch_limits
-    print_list(limits, :name, :amount, :max)
+    limits.each do |limit|
+      widest_name_size = limits.map { |l| l.name }.inject(0) { |max, name| name.size >= max ? name.size : max }
+      name = limit.name.ljust(widest_name_size)
+      limit_bar = make_limit_bar(limit.amount, limit.max)
+      amount = "#{limit.amount}zł z #{limit.max}zł"
+
+      puts "#{name} #{limit_bar} #{amount}"
+    end
+  end
+
+  private
+
+  def make_limit_bar(amount, max)
+    amount = [amount, max].min
+    taken = ((amount / max) * BAR_SIZE).to_i
+
+    bar = "["
+    taken.times do
+      bar << "#"
+    end
+
+    (BAR_SIZE - taken).times do
+      bar << "."
+    end
+    bar << "]"
+
+    return bar
   end
 
 end
